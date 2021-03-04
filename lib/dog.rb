5c031742909eca4efaeeb74dbc6160988e8eb98a
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-04 17:08:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
   
   def self.new_from_db(row)
     attr_hash = {id: row[0], name: row[1], breed: row[2]}
-    Dog.new(attr_hash)
+    self.new(attr_hash)
   end
   
   def self.find_by_id(id)
